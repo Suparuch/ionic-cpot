98a9fbd68e94b39ff89328024b64b22a90dcc7e2
suparuch <suparuch.che@gmail.com>
2019-01-10 15:07:53 +0700

    change flow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,39 +1,34 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProductItemPage } from '../product-item/product-item';
 
+@IonicPage({
+  name: 'home-page',
+  segment: 'home'
+})
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public actionSheetCtrl: ActionSheetController) { }
+  items = [
+    { name: 'ผ้าและผลิตภัณฑ์จากผ้า', img: "http://thailandshoppingparadise.com/uploads/images/SA_11_silk.png" },
+    { name: 'ของใช้ตกแต่ง', img: "https://www.forfur.com/img/I56/l_5526_14582223291155301584.jpg" },
+    { name: 'เครื่องประดับ', img: "http://www.weddingsquare.com/uploads/68294/%E0%B8%9E%E0%B8%B8%E0%B8%94%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%9A%E0%B8%B8%E0%B8%A8%E0%B8%A2%E0%B9%8C1.jpg" },
+    { name: 'อาหาร', img: "https://mpics.mgronline.com/pics/Images/560000003537902.JPEG" },
+  ]
 
-  openMenu() {
-    const actionSheet = this.actionSheetCtrl.create({
-      title: 'Modify',
-      buttons: [
-        {
-          text: 'Destructive',
-          role: 'destructive',
-          handler: () => {
-            console.log('Destructive clicked');
-          }
-        },{
-          text: 'Archive',
-          handler: () => {
-            console.log('Archive clicked');
-          }
-        },{
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('xxx');
-          }
-        }
-      ]
-    });
-    actionSheet.present();
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+  }
+
+  onClick(item) {
+    this.navCtrl.push(ProductItemPage, {
+      name: item.name,
+      imgPath: item.img
+    });
+  }
 }
