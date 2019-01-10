d6f9525bf3f907285d8dbd092801e307a05c492a
suparuch <suparuch.che@gmail.com>
2019-01-10 15:34:48 +0700

    add page product login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { ProductItemPage } from '../pages/product-item/product-item';
+
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    ProductItemPage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { LoginPage } from '../pages/login/login';
   entryComponents: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    ProductItemPage
   ],
   providers: [
     StatusBar,
